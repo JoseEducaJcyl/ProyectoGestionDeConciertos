f6d1ca919125e540a17635534e0657ab9f436ec6
JoseEducaJcyl <josea.morgut.1@educa.jcyl.es>
2026-04-21 19:45:00 +0200

    Comentarios agregados

--- src/Entrada.java
+++ src/Entrada.java
@@ -1,18 +1,26 @@
+// Clase Entrada que representa la compra de entradas para un concierto
+// Contiene los atributos básicos y sus métodos getter/setter
 public class Entrada {
-    private int id;
-    private int concierto;
-    private String comprador;
-    private int cantidad;
-    private String fechaCompra;
+    // Atributos privados de la clase (encapsulación)
+    private int id;                 // Identificador único de la compra/entrada
+    private int concierto;          // ID del concierto al que se asiste (referencia a Concierto)
+    private String comprador;       // Nombre de la persona que compra las entradas
+    private int cantidad;           // Número de entradas adquiridas
+    private String fechaCompra;     // Fecha en que se realizó la compra
 
+    // Constructor con todos los parámetros para inicializar un objeto Entrada
     public Entrada(int id, int concierto, String comprador, int cantidad, String fechaCompra) {
         this.id = id;
-        this.concierto = concierto;
+        this.concierto = concierto;   // Almacena el ID del concierto (FK hacia Concierto)
         this.comprador = comprador;
         this.cantidad = cantidad;
         this.fechaCompra = fechaCompra;
     }
 
+    // Métodos Getter y Setter (accesadores y mutadores)
+    // Permiten acceder y modificar los atributos privados de forma controlada
+
+    // Getter y Setter para id
     public int getId() {
         return id;
     }
@@ -21,6 +29,7 @@ public class Entrada {
         this.id = id;
     }
 
+    // Getter y Setter para concierto (almacena el ID del concierto)
     public int getConcierto() {
         return concierto;
     }
@@ -29,6 +38,7 @@ public class Entrada {
         this.concierto = concierto;
     }
 
+    // Getter y Setter para comprador
     public String getComprador() {
         return comprador;
     }
@@ -37,6 +47,7 @@ public class Entrada {
         this.comprador = comprador;
     }
 
+    // Getter y Setter para cantidad
     public int getCantidad() {
         return cantidad;
     }
@@ -45,6 +56,7 @@ public class Entrada {
         this.cantidad = cantidad;
     }
 
+    // Getter y Setter para fechaCompra
     public String getFechaCompra() {
         return fechaCompra;
     }
